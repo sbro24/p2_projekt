4f04a398bb6f1a752da4a4934b589b50b10c29c9
Simon Bro <sib24@student.aau.dk>
2025-04-10 12:00:48 +0200

    initial arima commit

---
+++ ArimaForecast.js
@@ -0,0 +1,19 @@
+const ARIMA = require('arima')
+
+function read_from_database()
+
+function format_data()
+
+function write_to_database()
+
+function get_q()
+
+function get_d()
+
+function get_p()
+
+function calculate_forecast(){
+    const arima = new ARIMA('p,d,q')
+}
+
+
