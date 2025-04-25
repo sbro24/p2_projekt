2a4599d55d3bbc8f4cedeae2a0f511a9c7ae2da2
Cilas <Cilas@DominoWWW.com>
2025-04-25 18:54:44 +0200

    Cleaned logging feature

--- src/app/server.js
+++ src/app/server.js
@@ -1,23 +1,23 @@
 import http from 'http';
 import fs from "fs";
-import path  from "path";
 import process from 'process';
 
-export { Log }
+import { CreateLogFile, Log } from '../lib/logging/log.js';
+import { StartTimestamp } from '../lib/time/time.js';
 
+StartTimestamp();
 const hostname = '127.0.0.1';
 const port = 4220;
 
-const startupTime  = new Date(); //format: YYYY-MM-DDTHH:mm:ss.sssZ
-const srcDcirectory = process.cwd() + '/src';
 const logDcirectory = process.cwd() + '/logs';
+
 
 const server = http.createServer(RequestHandler);
 function RequestHandler(req,res) {
     try {
         //processReq(req, res);
+        Log(req);
         if (req.url === '/') {
-            Log(req);
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.writeHeader(200, {
                 'Content-Type': 'application/json'
@@ -26,65 +26,10 @@ function RequestHandler(req,res) {
             res.end();
         }
     } catch(err) {
-        console.log(err);
+        Log(err);
         //errorResponse(res,500,"");
     };
 };
-
-function getTimestamp(timestamp) {
-    //check if timestamp is a date object
-    if (!(timestamp instanceof Date)) return undefined;
-    //format : YYYY-MM-DD
-    const date = timestamp.toISOString().split('T')[0];
-    //format : hh-mm-ss
-    const time = timestamp.toISOString().split('T')[1].split('.')[0].replace(/:/g, '-');
-    //format : YYYY-MM-DD_hh-mm-ss
-    return date + '_' + time;
-}
-
-function Log(message) {
-    const currentTime = new Date();
-    const timestamp = getTimestamp(startupTime);
-    const timeWithColon = currentTime.toISOString().split('T')[1].split('.')[0];
-    const filename = timestamp + '.log';
-    const filepath = logDcirectory + '/' + filename;
-    const timestampBox = `[${timeWithColon}]: `
-    console.log(timestampBox + message);
-    fs.appendFile(filepath, timestampBox + message + '\n', (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });  
-}
-
-function CreateLogFile() {
-    //create log directory it dosen't exist
-    const timestamp = getTimestamp(startupTime);
-    const timeWithColon = startupTime.toISOString().split('T')[1].split('.')[0];
-    if (!fs.existsSync(logDcirectory)) {
-        try {
-            console.log(`[${timeWithColon}]: Creating log directory...`);
-            fs.mkdirSync(logDcirectory, { recursive: true });
-        } catch (err) {
-            console.error(err);
-            process.exit(1);
-        }
-    }
-
-    const filename = timestamp + '.log';
-    const filepath = logDcirectory + '/' + filename;
-    //generate log directory if not exists
-    if (!fs.existsSync(filename)) {
-        try {
-            const content = `[${timeWithColon}]: Creating log file...`
-            console.log(content);
-            fs.writeFileSync(filepath, content + '\n', { flag: 'a' });
-          } catch (err) {
-            console.error(err);
-            process.exit(1);
-          }
-    }
-}
 
 function StartServer() {
     CreateLogFile();
---
+++ src/lib/time/time.js
@@ -0,0 +1,104 @@
+//This function returns the timestamp the application started running.
+var startTimestamp;
+export function StartTimestamp() {
+    if (!startTimestamp) startTimestamp = new Date();
+    return startTimestamp;
+};
+
+//format: $YYYY-$MM-$DD $HH:$mm:$ss
+export function TimestampFormatter(timestamp, format) {
+    //return undefined if arguments are the wrong type.
+    if (!(timestamp instanceof Date) || typeof format !== 'string') return undefined;
+
+    let formattedDate = '';
+
+    //Go for each character in string.
+    for (let i = 0; i < format.length; i++) {
+        //Escape character.
+        if (format[i] === '\\') {
+            i++;
+            formattedDate += format[i];
+            continue;
+        }
+
+        //year
+        if (format.substring(i, i + 4) === 'YYYY') {
+            formattedDate += timestamp.getFullYear();
+            i += 3;
+            continue
+        }
+
+        //month leading zero
+        if (format.substring(i, i + 2) === 'MM') {
+            if (timestamp.getMonth() + 1 < 10) formattedDate += '0'; //add leading zero.
+            formattedDate += timestamp.getMonth() + 1;
+            i += 1;
+            continue;
+        }
+
+        //month
+        if (format[i] === 'M') {
+            formattedDate += timestamp.getMonth() + 1;
+            continue;
+        }
+
+        //day leading zero
+        if (format.substring(i, i + 2) === 'DD') {
+            if (timestamp.getDate() + 1 < 10) formattedDate += '0'; //add leading zero.
+            formattedDate += timestamp.getDate();
+            i += 1;
+            continue;
+        }
+
+        //day
+        if (format[i] === 'D') {
+            formattedDate += timestamp.getDate();
+            continue;
+        }
+        
+        //hour leading zero
+        if (format.substring(i, i + 2) === 'HH') {
+            if (timestamp.getHours() < 10) formattedDate += '0'; //add leading zero.
+            formattedDate += timestamp.getHours();
+            i += 1;
+            continue;
+        }
+
+        //hour
+        if (format[i] === 'H') {
+            formattedDate += timestamp.getHours();
+            continue;
+        }
+
+        //minutes leading zero
+        if (format.substring(i, i + 2) === 'mm') {
+            if (timestamp.getMinutes() < 10) formattedDate += '0'; //add leading zero.
+            formattedDate += timestamp.getMinutes();
+            i += 1;
+            continue;
+        }
+
+        //minutes
+        if (format[i] === 'm') {
+            formattedDate += timestamp.getMinutes();
+            continue;
+        }
+
+        //seconds leading zero
+        if (format.substring(i, i + 2) === 'ss') {
+            if (timestamp.getSeconds() < 10) formattedDate += '0'; //add leading zero.
+            formattedDate += timestamp.getSeconds();
+            i += 1;
+            continue;
+        }
+
+        //seconds
+        if (format[i] === 's') {
+            formattedDate += timestamp.getSeconds();
+            continue;
+        }
+
+        formattedDate += format[i]; //add the character to the string.
+    }
+    return formattedDate;
+};
