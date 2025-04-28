0d316cad201d8deec66f8c33cde8de55c50f42de
DominoWWW <Cilas@DominoWWW.com>
2025-04-28 16:59:42 +0200

    Add comment

--- src/lib/errorHandling/error.js
+++ src/lib/errorHandling/error.js
@@ -6,6 +6,7 @@ export function ErrorToString(err) {
     if (err instanceof Error) {
         let cause = '';
         if (err.cause !== undefined) cause = `\n  [cause]: '${err.cause}'`;
+        //slice(7) removes the first 7 characters of the stack trace: "Error: "
         return err.stack.slice(7) + cause;
     }
 }
