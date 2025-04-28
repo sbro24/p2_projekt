54c0c8cbc82b234f74a7ca53b3b3234479ebe58d
Skrimma <hhjart24@student.aau.dk>
2025-04-28 12:10:42 +0200

    ARIMA progress

--- ArimaForecast.js
+++ ArimaForecast.js
@@ -1,26 +1,59 @@
 const ARIMA = require('arima')
 
-function read_from_database()
+function ReadFromDatabase()
 
-function format_data()
+function FormatData()
 
-function write_to_database()
+function WriteToDatabase()
 
-function get_d()
+function Get_d()
 
-function calcAIC() {
-let observed = [3, 5, 7, 9, 2]
+/** Calculates the residual sum of squares (RSS) for the AIC
+    inputs:
+    data = the data used to fit the model
+    predicted = the predicted values from the model
+    Output:
+    rss = the residual sum of squares (RSS)
+*/
+function CalcRSS(data, predicted) { 
+    data = [3, 5, 7, 9, 2] // Example data values
+    predicted = [3.5, 4.5, 6.5, 9.5, 2.5] // Example predicted valuess
 
-let predicted = [3.5, 4.5, 6.5, 9.5, 2.5]
-
-    function get_p()
-
-    function get_q()
+    const mean = data.reduce((sum, value) => sum + value, 0) / data.length 
+    const rss = observed.reduce((sum, dataValue, i) => {
+        const residual = dataValue - predicted[i]; // Calculate the residual for each observation
+        return sum + (residual)**2; // Calculates the residual sum of squares (RSS)
+    }, 0) // 0 is the start value of the sum
+        
+    return rss;
 }
 
+/**  Calculates the (AIC) for parameter selection 
+    inputs:
+    obs = All of the observed months of data
+    k = number of parameters in the model
+    Output: AIC of the given ARIMA model
+*/
+function CalcAIC(obs, k) { 
+    const observed = obs.length
+    const rss = CalcRSS() // Call the function to calculate RSS
+    const sigmaSquared = rss /  // sigma squared is the residual variance
+    const logLikelihood = -(n / 2) * (Math.log(2 * Math.PI)) -(n / 2) * (Math.log(sigmaSquared)) -(1 / (2 * sigmaSquared)) * rss // Calculates the log-likelihood
+    const AIC = -2 * logLikelihood + 2 * k // Calculates the AIC
+    return AIC;
+}
+
+function SelectBestModel(data, AIC) {
+    
+}
+
+function Get_p()
+
+function Get_q()
 
 
-function calculate_forecast(parameters=[], data, steps){
+function CalculateForecast(parameters=[], data, steps) {
+
     const arima = new ARIMA(parameters).train(data)
     const [prediction, errors] = arima.predict(steps)
 }
