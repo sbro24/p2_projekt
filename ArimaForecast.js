31dda1d25eb179a9927ae5e04985431958a1acbf
Skrimma <hhjart24@student.aau.dk>
2025-04-29 12:12:43 +0200

    ARIMA progress 3

--- ArimaForecast.js
+++ ArimaForecast.js
@@ -1,6 +1,24 @@
 const ARIMA = require('arima')
 
-data = [5, 3, 7, 2, 7, 4, 6, 8, 4, 9] // Test data
+class forecast {
+    constructor(model) {
+    class model {
+        constructor(data, order, AIC) {
+            this.data = data // The financial data given
+            this.order = order // The order of the ARIMA model
+            this.AIC = AIC // The AIC of the ARIMA model
+        }
+    } 
+    }
+}
+
+// Test data
+const data = [
+    102.3, 101.7, 103.5, 104.1, 105.6, 106.2, 107.0, 108.5, 109.8, 111.1, 110.5, 112.0,
+    113.4, 114.0, 113.2, 114.9, 115.7, 117.3, 118.1, 119.0, 118.6, 120.4, 121.7, 123.3,
+    122.8, 124.6, 125.0, 126.5, 127.3, 128.0, 129.1, 130.5, 132.2, 131.8, 133.0, 134.4,
+    135.1, 136.6, 137.3, 136.8, 138.2, 139.9, 141.0, 140.4, 142.1, 143.5, 144.3, 145.6
+  ];   
 
 function ReadFromDatabase()
 
@@ -12,12 +30,13 @@ function Get_d()
 
 /** Calculates the residual sum of squares (RSS), to be used by the AIC
     @param {data = the financial data given}
-    @param {predicted = the predicted values from the model}
+    @param {forecast = the predicted values from the model}
     Output: rss = the residual sum of squares (RSS)
 */
-function CalcRSS(data, predicted) { // Calculate the residual sum of squares (RSS)
+function CalcRSS(data, forecast) { // Calculate the residual sum of squares (RSS)
+    const recentData = data.slice(data.length - forecast.length) // Get the most recent data points to compare with the forecast
     const rss = data.reduce((sum, dataValue, i) => {
-        const residual = dataValue - predicted[i]; // Calculate the residual for each observation
+        const residual = dataValue - recentData[i]; // Calculate the residual for each observation
         return sum + (residual)**2; // Calculates the residual sum of squares (RSS)
     }, 0) // 0 is the start value of the sum
         
@@ -26,14 +45,13 @@ function CalcRSS(data, predicted) { // Calculate the residual sum of squares (RS
 
 /**  Calculates the (AIC) for parameter selection 
     @param {data = the financial data given}
-    @param {p = the AR parameter}
-    @param {q = the MA parameter}
-    @param {c = the constant, if included in the model}
+    @param {config = the order of the ARIMA model}
+    @param {forecast = the predicted values from the model}
     Output: AIC of the given ARIMA model
 */
-function CalcAIC(config, data) { 
-    const n = data.length // Total number of observations
-    const rss = CalcRSS(data) // Call the function to calculate RSS
+function CalcAIC(data, config, forecast) { // Calculate the AIC for the given ARIMA model
+    const n = data.slice(data.length - forecast.length) // Number of observations used for RSS
+    const rss = CalcRSS(data, forecast) // Call the function to calculate RSS
     let k = config.p + config.q  // the amount of parameters in the model
         if (config.c) { // If a constant is included in the model, add 1 to k
             k += 1
@@ -46,40 +64,37 @@ function CalcAIC(config, data) {
 
 
 
-function SelectBestModel(data) {
+function SelectOrder(data) {
     const d = Get_d(data) // Call the function to get the differencing order
     for (let c = 0; c <= 1; c++) { // Loop through, to check if the constant should be included
-        for (let p = 0; p <= 5; p++) { // Loop through the AR parameters
-            for (let q = 0; q <= 5; q++) { // Loop through the MA parameters
+        for (let p = 0; p <= 5; p++) { // Loop through the AR orders
+            for (let q = 0; q <= 5; q++) { // Loop through the MA orders
                 const bestAIC = Infinity // Initialize the best AIC to infinity
-                let bestModel = [] // Initialize the best model parameters'
+                let bestOrder = [] // Initialize the best model
                 let config; // Initialize config variable
-                if (c === 0) {
-                    config = { p, d, q, c: false} // Sets the order of the ARIMA model to the current parameters
-                } else {
-                    config = { p, d, q, c: true} // Sets the order of ARIMA model to the current parameters and a constant
-                }    
-                const ARIMA = new ARIMA(config).train(data) // Create a new ARIMA model using the config
-                const forecast = ARIMA.predict(data.length) // Predict the next value using the ARIMA model
-                const aic = CalcAIC(forecast, config, data) // 
-                    
+                    if (c === 0) {
+                        config = { p, d, q, c: false} // Sets the order of the ARIMA model to the current parameters
+                    } else {
+                        config = { p, d, q, c: true} // Sets the order of the ARIMA model to the current parameters and a constant
+                    }    
+
+                const arima = new ARIMA(config).train(data) // Create a new ARIMA model using the config
+                const testForecast = arima.predict(12) // Predict the next 12 months using the ARIMA model
+                const aic = CalcAIC(data, config, testForecast) // calculate the AIC of the current ARIMA model
+                        
                     if (aic < bestAIC) { // If the AIC is lower than the best AIC found so far
                         bestAIC = aic // Update the best AIC
-                        bestModel = [p, d, q] // Update the best model parameters
+                        bestOrder = [p, d, q] // Update the best model parameters
                     }
             }
         }
-    } 
+    }
+    return bestOrder;  
 }
 
-function Get_p()
-
-function Get_q()
-
-
-function CalculateForecast(parameters=[], data, steps) {
-    const arima = new ARIMA(parameters).train(data)
-    const [prediction, errors] = arima.predict(steps)
+function CalculateForecast(order, data) {
+    const arima = new ARIMA(order).train(data)
+    const [prediction, errors] = arima.predict(12)
 }
 
-
+CalculateForecast(config,)
