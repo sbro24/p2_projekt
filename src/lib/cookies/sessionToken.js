6ab92b4cacc69007debaf47844e27ddfce48942b
DominoWWW <Cilas@DominoWWW.com>
2025-05-02 15:40:48 +0200

    Add session token generation functionality

---
+++ src/lib/cookies/sessionToken.js
@@ -0,0 +1,19 @@
+
+
+
+function GenSessionToken() {
+    let result = '';
+    const n = 20
+    const chars = '1234567890abcdefghifklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_'
+    const min = 0
+    const max = chars.length - 1
+    for (let i = 0; i < n; i++) {
+        result += chars.charAt(randomIntFromInterval(min, max));
+    }
+    return result
+}
+console.log(GenSessionToken(20))
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
