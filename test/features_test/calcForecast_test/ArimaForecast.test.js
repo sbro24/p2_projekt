098fdc4a2d2699c849f1031f262c96f36e80c059
Simon Bro <sib24@student.aau.dk>
2025-05-14 11:58:18 +0200

    added arima test suite (in development)

--- test/features_test/calcForecast_test/ArimaForecast.test.js
+++ test/features_test/calcForecast_test/ArimaForecast.test.js
@@ -1,20 +1,20 @@
-const ARIMA = require('arima')
+console.log('Current working directory:', process.cwd());
+process.chdir('../src/features/calcForecast/');
+console.log('Changed working directory to:', process.cwd());
 
-function read_from_database()
-
-function format_data()
-
-function write_to_database()
-
-function get_q()
-
-function get_d()
-
-function get_p()
-
-function calculate_forecast(parameters=[], data, steps){
-    const arima = new ARIMA(parameters).train(data)
-    const [prediction, errors] = arima.predict(steps)
-}
+//const ArimaForecast = require('../src/features/calcForecast/ArimaForecast.js')
 
 
+const data = [  555866, 174701, 458500, 323800, 30750, 456900, 194000, 416297 ,303865, 448050, 178515, 168222,
+    333520, 330090, 325457, 367068, 327179, 247301, 340363, 553907, 361974, 242516, 124659, 223538,
+    108450, 306087, 340362, 388343, 448847, 107248, 428296, 324494, 922040, 
+    394335, 330600, 630237, 86555, 290790, 788550, 77700, 109050, 318261, 160350, 72089, 376368]; 
+
+//SelectOrder(data)
+// test exponential
+// test seasonal
+// test linear growth
+// test non-seasonal trends
+// test non-stationary cases
+// test real case
+// analyze best models 
