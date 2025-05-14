f897b3aa64bd09237c9204c17c909b3f82eb2c50
Cilas <Cilas@DominoWWW.com>
2025-05-14 13:37:10 +0200

    refactor: update user profile and data routes in global router

--- src/features/global/router.js
+++ src/features/global/router.js
@@ -1,7 +1,8 @@
 import { FileResponse, DataResponse } from "../../app/router.js";
-import {  GetFinancialDataById } from "../../lib/useDatabase/handle-data.js";
+import { GetCompanyProfileByToken, GetFinancialDataById } from "../../lib/useDatabase/handle-data.js";
+import { CheckAuth, GetSessionToken } from "../../lib/cookies/sessionToken.js";
 
-export function router(req, res, data) {
+export async function router(req, res, data) {
     switch (req.url) {
         case '/api/global/style':
             FileResponse(res, 'global/style.css');
@@ -12,6 +13,24 @@ export function router(req, res, data) {
         case '/api/global/gentabel':
             FileResponse(res, 'global/generateTables.js');
             break;
+        case '/api/user/profile':
+            if (await CheckAuth(req, res)) {
+                const token = GetSessionToken(req);
+                let company = await GetCompanyProfileByToken(token)
+                DataResponse(res, company);
+            } else {
+                FileResponse(res, 'login/needLogin.html');
+            }
+            break;
+        case '/api/user/data':
+            if (await CheckAuth(req, res)) {
+                const token = GetSessionToken(req);
+                let company = await GetCompanyProfileByToken(token)
+                let data = await GetFinancialDataById(company.id)
+                DataResponse(res, data);
+            } else {
+                FileResponse(res, 'login/needLogin.html');
+            }
         default:
             break;
     }
