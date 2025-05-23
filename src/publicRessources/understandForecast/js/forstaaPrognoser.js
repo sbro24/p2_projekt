1b5996409f867f1a3dbaf6fc1d95b92832580c0b
Mulvarperercool <xpouls24@student.aau.dk>
2025-05-23 20:09:46 +0200

    Static data on forstaaPrognoser charts

--- src/publicRessources/understandForecast/js/forstaaPrognoser.js
+++ src/publicRessources/understandForecast/js/forstaaPrognoser.js
@@ -1,59 +1,67 @@
 // "Read more" button
 document.getElementById("readMore").onclick = function () {
-  window.location.href = "../../global/html/about.html";
-  window.location.href = "api/global/about.html";
+    window.location.href = "/about/";
 };
-                    
-// "Opdater tabel" button - Revenue
-document.getElementById("updateChartRevenue").onclick = function () {
-                                
-};
-
-// "Opdater tabel" button - Expenses
-document.getElementById("updateChartExpenses").onclick = function () {
-                                
-};
-
 
 // Charts
 const xValues = ['Jan','Feb','Mar','Apr','Maj','Jun','Jul','Aug','Sep','Okt','Nov','Dec'];
 
 // Chart Revenue
 const chartRevenue = new Chart("chartRevenue", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{ 
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478,4732,1374], //hent rigtig data somehow
-      borderColor: "red",
-      fill: false
-    }, { 
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100,6000,4000], //hent rigtig data somehow
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{ 
+            label: 'Indtægt - Prognose',
+            data: [-2116498,-2359971,-988380,-1292124,-1595898,-1450928,-1046572,-1102130,-1352841,-1167317,-1989371,-2212521],
+            borderColor: "red",
+            borderwidth: 2,
+            fill: false
+        }, { 
+            label: 'Indtægt - Budget',
+            data: [-1876498,-2660192,-768383,-1234524,-1395898,-1452124,-1146572,-1302130,-1332841,-867317,-1489371,-2782749],
+            borderColor: "blue",
+            borderwidth: 2,
+            fill: false
+        }]
+    },
+    options: {
+        legend: {display: false}
+    },
+    scales: {
+        y: {
+            beginAtZero: false,
+            ticks: { callback: value => `${value.toLocaleString()} kr` }
+        }
+    }
 });
 
 // Chart Expenses
 const chartExpenses = new Chart("chartExpenses", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{ 
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478,4732,1374], //hent rigtig data somehow
-      borderColor: "red",
-      fill: false
-    }, { 
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100,6000,4000], //hent rigtig data somehow
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{ 
+            label: 'Udgifter - Prognose',
+            data: [1576498,1860192,1368383,1434524,1395898,1297851,1282345,1070630,1132841,1367317,1589371,1882749], 
+            borderColor: "red",
+            borderwidth: 2,
+            fill: false
+        }, { 
+            label: 'Udgifter - Budget',
+            data: [1876498,2660192,768383,1234524,1395898,1467851,1162345,1270630,1332841,867317,1489371,2782749], 
+            borderColor: "blue",
+            borderwidth: 2,
+            fill: false
+        }]
+    },
+    options: {
+        legend: {display: false}
+    },
+    scales: {
+        y: {
+            beginAtZero: false,
+            ticks: { callback: value => `${value.toLocaleString()} kr` }
+        }
+    }
 });
